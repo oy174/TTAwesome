4ccebf422f3f08820f4dd6ef5ea1b95c7e40a7fa
oy174 <mmfker@gmail.com>
2015-01-28 00:11:56 -0800

    init

--- TTAwesome.js
+++ TTAwesome.js
@@ -10,37 +10,20 @@ var board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
 // Creates X and O in variables
 var X = 'X', O = 'O';
-
-function showCurrentBoard()
-{
-	var currectBoard = board[0] + ' | ' + board[1] + ' | ' + board[2] + '\n'
-					 + '- -+- -+- - \n'
-					 + board[3] + ' | ' + board[4] + ' | ' + board[5] + '\n'
-					 + '- -+- -+- - \n'
-					 + board[6] + ' | ' + board[7] + ' | ' + board[8] + '\n';
-	process.stdout.write(currentBoard);
-}
-
 function pc()
 {
-	process.stdin.on('readable', function() 
-	{
-		process.stdout.write("Chose the number area you wish to make your move");
-		var UI = process.stdin.read();
-		if (UI == '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9') 
+	$(".ttt").click(function()
+	{	
+		var UI = $(this).id();
+		if(board[UI] == ' ')
 		{
-			UI = UI - 1;
-			if(board[UI] == ' ')
-			{
-				board[UI] = X;
-				process.exit();
-				else process.stdout.write("That space is taken make another choice");
-			}
-			else process.stdout.write("Please enter only numerical values from 1 to 9");
+			board[UI] = X;
+			$(this).text("X");
 		}
+		else alert("That space is taken make another choice");
 	}); 
-	showCurrentBoard();
 }
+	
 // NPC is dumb and will cycle through positions until an open one is found
 function npcAI()
 {
@@ -49,7 +32,7 @@ function npcAI()
 		while(board[i] == ' ');
 		{
 			board[i] = O;
-			showCurrentBoard();
+			document.body.table.i.innerHTML = O;
 			return;
 		}
 	}
@@ -68,7 +51,7 @@ function winCheck()
 		board[0] == board[4] == board[8] == 'X' ||
 		board[2] == board[4] == board[6] == 'X' )
 		{
-			process.stdout.write("Winning play");
+			alert("You win");
 		}
 }		
 
@@ -85,7 +68,7 @@ function loseCheck()
 		board[0] == board[4] == board[8] == 'O' ||
 		board[2] == board[4] == board[6] == 'O')
 		{
-			process.stdout.write("You Lose");
+			alert("You Lose");
 		}
 }
 
@@ -97,13 +80,12 @@ function tieCheck()
 		{
 			break;
 		}
-		process.stdout.write("Tie!");
+		alert("Tie!");
 	}	
 }
 
 function game()
 {	
-	showCurrentBoard();
 	for(var p = 0; p < 9; p += 1)
 	{
 		pc();
@@ -116,5 +98,5 @@ function game()
 			break;
 		}
 	}
-	process.stdout.write("Game Over!");
+	alert("Game Over!");
 }
